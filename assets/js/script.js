349be62e27aa05fe340227f8bcc478bf1709b3f4
Chris Biertzer <c.biertzer@gmail.com>
2020-09-23 15:08:18 -0500

    finished persistence feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -280,6 +280,20 @@ var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+var loadTasks = function() {
+    var savedTasks = localStorage.getItem("tasks");
+
+    if (!savedTasks) {
+        return false;
+    }
+
+    savedTasks = JSON.parse(savedTasks);
+
+    for (var i = 0; i < savedTasks.length; i++) {
+        createTaskEl(savedTasks[i]);
+    }
+}
+
 pageContentEl.addEventListener("click", taskButttonHandler);
 
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
@@ -292,4 +306,4 @@ pageContentEl.addEventListener("drop", dropTaskHandler);
 
 pageContentEl.addEventListener("dragleave", dragLeaveHandler);
 
-
+loadTasks();
